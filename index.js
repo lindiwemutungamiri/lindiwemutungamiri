422737f5d24f4e0bbb2f748ebdd5b795526d4ffa
Lindiwe Mutungamiri <lindiwemutungamiri23@gmail.com>
2022-08-06 12:36:09 +0000

    check

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
   * Notice the "name" and "date" property.
 */
 let DATA = {
-  name: 'Lindiwe',
+  name: 'Lindiwe Mutungamiri',
   date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
